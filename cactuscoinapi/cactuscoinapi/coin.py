0f81a13118a1b5272b00683c974fe9901591ea72
cybaix <cybaix@protonmail.com>
2018-08-25 16:39:39 -0600

    added get support for the coin endpoint to return a list of connected badges

--- cactuscoinapi/cactuscoinapi/coin.py
+++ cactuscoinapi/cactuscoinapi/coin.py
@@ -1,10 +1,24 @@
 import json
+import time
+
 from flask import current_app as app
 from flask_restful import abort, Resource
 from .utils import get_signed_json, signed_jsonify
 from .crypto import InvalidMessageSignature, verify
 
 class Coin(Resource):
+    def get(self, badge_id):
+        coins = []
+        badges = app.redis_store.smembers('badge_coins_{}'.format(badge_id))
+
+        for other_badge in badges:
+            other_id = int(other_badge)
+            other_name = app.redis_store.hget('badge_names', other_badge).decode('utf8')
+            timestamp = int(app.redis_store.hget('coins', str(sorted((badge_id, other_id)))))
+            coins.append({'other_id':other_id, 'other_name':other_name, 'timestamp':timestamp})
+
+        return signed_jsonify(coins)
+
     def post(self, badge_id):
         coin = get_signed_json(badge_id)
 
@@ -18,14 +32,27 @@ class Coin(Resource):
         # check if a coin already exists
         keys = sorted((csr['beacon_id'], csr['seer_id']))
 
-        if app.redis_store.sismember('coins', keys):
+        if app.redis_store.hget('coins', keys):
             abort(409, message=signed_jsonify({'status':208, 'message':'coin already submitted'}))
 
-        app.redis_store.sadd('coins', keys)
+        app.redis_store.hset('coins', keys, int(time.time()))
 
-        # save coin
+        # link badges... TODO: handle failure better, e.g. one badge links, other fails
+        app.redis_store.sadd('badge_coins_{}'.format(csr['beacon_id']), csr['seer_id'])  
+        app.redis_store.sadd('badge_coins_{}'.format(csr['seer_id']), csr['beacon_id'])  
 
     def validate(self, coin):
+        """ Checks the signatures on the coin for the badges involved. Looks up appropriate badge
+        keys based on the IDs supplied in the initial coin signing request (CSR).  Please note
+        that it is required for the beacon signature to cover the csr and seer_sig.
+
+        Args:
+            coin: JSON message containing the full cactuscoin. Should look something like:
+                  {"csr": {"beacon_id":1, "seer_id":2}, "seer_sig":"base64", "beacon_sig":"base64"}
+
+        Raises:
+            cactuscoin.crypto.InvalidMessageSignature: When an invalid signature is detected on the coin.
+        """
         csr = json.loads(coin['csr'])
         beacon_sig = coin.pop('beacon_sig')
         # validate beacon signature (coin, beacon_sig)
